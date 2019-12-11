04286c305c38843a1bde53bb4c7dfe7a9ec77570
Douglas Hammarstam <dogge20@hotmail.com>
2019-12-11 12:47:55 +0100

    works a litte

--- src/assign2prop/core.clj
+++ src/assign2prop/core.clj
@@ -4,7 +4,10 @@
 
 ;If an exception is thrown inside the expression the exception should be returned.
 ;Otherwise the evaluated value of the expression should be returned. If a binding is
-;provided it should be possible to use the bound variable inside the expression. After
+;provided it should be possible to use the bound variable inside the expression.
+;
+;
+; After
 ;execution of the form any variables that are bound shall be closed (e.g.
 ; (let [s (Socket.)] (. s close))) Note that closeable classes in Java implements
 ;the Closeable–interface (hint: use type hints). The return value of the macro
@@ -18,21 +21,36 @@
 
 (defmacro safe [vect & expr]
 
-  (let [except `(catch Exception e# e#)]
-  `(let ~vect (try ~@expr ~except)
-              `(if (instance? java.io.Closeable `(first vect)) (.close `(first vect) ))
+  ; utvärdera expression och kasta exception ifall det finns ett exception
 
+  ;`(try (let ~vect) (catch Exception e# e#))
+
+  `(let ~vect (try ~@expr (catch Exception e# e#))          ; här binds värdet s till vektorn
 
               )
-   )
+
+  )
+
+(defmacro safe [expr]
+
+  ; utvärdera expression och kasta exception ifall det finns ett exception
+
+  `(try ~expr (catch Exception e# (str "Caught exception " e#)))
+
+  ;`(let ~vect (try ~@expr (catch Exception e# e#))          ; här binds värdet s till vektorn
 
   )
 
 
+
+
+
+
+
 (defn -main []
   ;(def s (FileReader. (File. "C:\\Users\\dogge\\IdeaProjects\\assign2prop\\src\\assign2prop\\text.txt")))
-  ;(def v (safe [1 2] (/ 1 0)))
-  (def v (safe [s (FileReader. (File. "C:\\Users\\dogge\\IdeaProjects\\assign2prop\\src\\assign2prop\\text.txt"))] (.read s)))
+  (def v (safe (/ 1 0)))
+  (def v (safe [s (FileReader. (File. "C:\\Usedsrs\\dogge\\IdeaProjects\\assign2prop\\src\\assign2prop\\text.txt"))] (.read s)))
   (println v)
   )
 
